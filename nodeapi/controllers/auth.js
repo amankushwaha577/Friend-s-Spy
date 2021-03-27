36b278ec0aa9849c95bb5b55082c0c5f508f49b5
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2021-03-28 00:08:59 +0530

    Create auth.js

---
+++ nodeapi/controllers/auth.js
@@ -0,0 +1,7 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const expressJwt = require('express-jwt');
+const User = require('../models/user');
+const _ = require('lodash');
+const { OAuth2Client } = require('google-auth-library');
+const { sendEmail } = require('../helpers');
