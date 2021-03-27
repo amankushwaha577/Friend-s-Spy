427f356b3b7eacdbbdf4bc90a88d4bcf4d353bc1
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2021-03-28 00:09:33 +0530

    Update auth.js

--- nodeapi/controllers/auth.js
+++ nodeapi/controllers/auth.js
@@ -5,3 +5,15 @@ const User = require('../models/user');
 const _ = require('lodash');
 const { OAuth2Client } = require('google-auth-library');
 const { sendEmail } = require('../helpers');
+
+
+exports.signup = async (req, res) => {
+    const userExists = await User.findOne({ email: req.body.email });
+    if (userExists)
+        return res.status(403).json({
+            error: 'Email is taken!'
+        });
+    const user = await new User(req.body);
+    await user.save();
+    res.status(200).json({ message: 'Signup success! Please login.' });
+};
