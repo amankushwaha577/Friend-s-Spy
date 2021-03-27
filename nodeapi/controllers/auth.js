fce013f56103b7239bac9d9ff0711f858f8aebc9
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2021-03-28 00:10:10 +0530

    Update auth.js

--- nodeapi/controllers/auth.js
+++ nodeapi/controllers/auth.js
@@ -17,3 +17,33 @@ exports.signup = async (req, res) => {
     await user.save();
     res.status(200).json({ message: 'Signup success! Please login.' });
 };
+
+
+
+
+exports.signin = (req, res) => {
+    // find the user based on email
+    const { email, password } = req.body;
+    User.findOne({ email }, (err, user) => {
+        // if err or no user
+        if (err || !user) {
+            return res.status(401).json({
+                error: 'User with that email does not exist. Please signup.'
+            });
+        }
+        // if user is found make sure the email and password match
+        // create authenticate method in model and use here
+        if (!user.authenticate(password)) {
+            return res.status(401).json({
+                error: 'Email and password do not match'
+            });
+        }
+        // generate a token with user id and secret
+        const token = jwt.sign({ _id: user._id, role: user.role }, process.env.JWT_SECRET);
+        // persist the token as 't' in cookie with expiry date
+        res.cookie('t', token, { expire: new Date() + 9999 });
+        // retrun response with user and token to frontend client
+        const { _id, name, email, role } = user;
+        return res.json({ token, user: { _id, email, name, role } });
+    });
+};
