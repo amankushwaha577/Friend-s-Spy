80a01610e8594056902a3fa30878e07ad8e18f00
amanS2206 <aman.saxena_cs18@gla.ac.in>
2021-04-08 17:53:55 +0530

    update photo

--- nodeapi/routes/post.js
+++ nodeapi/routes/post.js
@@ -38,3 +38,6 @@ router.get('/posts/by/:userId', requireSignin, postsByUser);
 router.get('/post/:postId', singlePost);
 router.put('/post/:postId', requireSignin, isPoster, updatePost);
 router.delete('/post/:postId', requireSignin, isPoster, deletePost);
+
+// photo
+router.get('/post/photo/:postId', photo);
