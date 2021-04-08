ddf15677e0db2be039c723cefbc00ba984117b77
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2021-04-08 14:57:48 +0530

    Update user.js

--- nodeapi/controllers/user.js
+++ nodeapi/controllers/user.js
@@ -36,3 +36,15 @@ exports.hasAuthorization = (req, res, next) => {
     }
     next();
 };
+
+
+exports.allUsers = (req, res) => {
+    User.find((err, users) => {
+        if (err) {
+            return res.status(400).json({
+                error: err
+            });
+        }
+        res.json(users);
+    }).select('name email updated created role');
+};
