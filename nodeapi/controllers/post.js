ee3e3074319256d5d15c9d61c493aafc5eebf87f
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2021-04-08 14:53:44 +0530

    Update post.js

--- nodeapi/controllers/post.js
+++ nodeapi/controllers/post.js
@@ -215,3 +215,22 @@ exports.photo = (req, res, next) => {
 exports.singlePost = (req, res) => {
     return res.json(req.post);
 };
+
+
+
+
+
+
+exports.like = (req, res) => {
+    Post.findByIdAndUpdate(req.body.postId, { $push: { likes: req.body.userId } }, { new: true }).exec(
+        (err, result) => {
+            if (err) {
+                return res.status(400).json({
+                    error: err
+                });
+            } else {
+                res.json(result);
+            }
+        }
+    );
+};
