96bf421f78dbd908aaa488e8042655e023af8319
amanS2206 <aman.saxena_cs18@gla.ac.in>
2021-04-08 17:57:17 +0530

    update user.js

--- nodeapi/routes/user.js
+++ nodeapi/routes/user.js
@@ -20,3 +20,6 @@ const router = express.Router();
 
 router.put("/user/follow", requireSignin, addFollowing, addFollower);
 router.put("/user/unfollow", requireSignin, removeFollowing, removeFollower);
+
+router.get("/users", allUsers);
+router.get("/user/:userId", requireSignin, getUser);
