ee8b8f5b8eae33ca6222aa818ca61686a98811c0
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2021-04-08 16:49:23 +0530

    Update app.js

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -9,3 +9,20 @@ const fs = require('fs');
 const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
+
+
+
+// db
+// mongodb://kaloraat:dhungel8@ds257054.mlab.com:57054/nodeapi
+// MONGO_URI=mongodb://localhost/nodeapi
+// mongodb+srv://kaloraat_admin:kkkkkk9@nodeapi-pbn7j.mongodb.net/nodeapi?retryWrites=truenodeAPI?retryWrites=true
+// mongodb+srv://robertchou_admin:Aeiourc2491@nodeapi-p2o93.mongodb.net/nodeapi?retryWrites=true&w=majority
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true
+    })
+    .then(() => console.log('DB Connected'));
+
+mongoose.connection.on('error', err => {
+    console.log(`DB connection error: ${err.message}`);
+});
