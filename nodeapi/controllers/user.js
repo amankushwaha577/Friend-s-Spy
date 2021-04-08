5913164e669b3950bbcacaf164679e2b973c97a6
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2021-04-08 14:58:45 +0530

    Update user.js

--- nodeapi/controllers/user.js
+++ nodeapi/controllers/user.js
@@ -71,3 +71,48 @@ exports.getUser = (req, res) => {
 //         res.json({ user });
 //     });
 // };
+
+
+
+
+
+
+
+
+
+exports.updateUser = (req, res, next) => {
+    let form = new formidable.IncomingForm();
+    // console.log("incoming form data: ", form);
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            return res.status(400).json({
+                error: 'Photo could not be uploaded'
+            });
+        }
+        // save user
+        let user = req.profile;
+        // console.log("user in update: ", user);
+        user = _.extend(user, fields);
+
+        user.updated = Date.now();
+        // console.log("USER FORM DATA UPDATE: ", user);
+
+        if (files.photo) {
+            user.photo.data = fs.readFileSync(files.photo.path);
+            user.photo.contentType = files.photo.type;
+        }
+
+        user.save((err, result) => {
+            if (err) {
+                return res.status(400).json({
+                    error: err
+                });
+            }
+            user.hashed_password = undefined;
+            user.salt = undefined;
+            // console.log("user after update with formdata: ", user);
+            res.json(user);
+        });
+    });
+};
