b35b1c89e6e97dce07cbb007d24a20837312ed2f
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2021-04-08 14:52:30 +0530

    Update post.js

--- nodeapi/controllers/post.js
+++ nodeapi/controllers/post.js
@@ -120,3 +120,21 @@ exports.postsByUser = (req, res) => {
             res.json(posts);
         });
 };
+
+
+exports.isPoster = (req, res, next) => {
+    let sameUser = req.post && req.auth && req.post.postedBy._id == req.auth._id;
+    let adminUser = req.post && req.auth && req.auth.role === 'admin';
+
+    // console.log("req.post ", req.post, " req.auth ", req.auth);
+    // console.log("SAMEUSER: ", sameUser, " ADMINUSER: ", adminUser);
+
+    let isPoster = sameUser || adminUser;
+
+    if (!isPoster) {
+        return res.status(403).json({
+            error: 'User is not authorized'
+        });
+    }
+    next();
+};
