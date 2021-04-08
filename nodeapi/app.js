88b96c247afb7fbb87585c50e0990a6c38977826
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2021-04-08 16:49:42 +0530

    Update app.js

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -26,3 +26,20 @@ mongoose
 mongoose.connection.on('error', err => {
     console.log(`DB connection error: ${err.message}`);
 });
+
+// bring in routes
+const postRoutes = require('./routes/post');
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+// apiDocs
+app.get('/api', (req, res) => {
+    fs.readFile('docs/apiDocs.json', (err, data) => {
+        if (err) {
+            res.status(400).json({
+                error: err
+            });
+        }
+        const docs = JSON.parse(data);
+        res.json(docs);
+    });
+});
