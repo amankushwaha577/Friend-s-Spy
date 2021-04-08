1d8d42ef9bbb2c11217714b73050d38bab3e8084
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2021-04-08 14:57:00 +0530

    Create user.js

---
+++ nodeapi/controllers/user.js
@@ -0,0 +1,20 @@
+const _ = require('lodash');
+const User = require('../models/user');
+const formidable = require('formidable');
+const fs = require('fs');
+
+exports.userById = (req, res, next, id) => {
+    User.findById(id)
+        // populate followers and following users array
+        .populate('following', '_id name')
+        .populate('followers', '_id name')
+        .exec((err, user) => {
+            if (err || !user) {
+                return res.status(400).json({
+                    error: 'User not found'
+                });
+            }
+            req.profile = user; // adds profile object in req with user info
+            next();
+        });
+};
