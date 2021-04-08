4b1be5df7bccc6269a2829a2586cadde991177ae
amanS2206 <aman.saxena_cs18@gla.ac.in>
2021-04-08 17:46:52 +0530

    import password reset validator

--- nodeapi/routes/auth.js
+++ nodeapi/routes/auth.js
@@ -1,2 +1,12 @@
 const express = require('express');
 const { signup, signin, signout, forgotPassword, resetPassword, socialLogin } = require('../controllers/auth');
+
+// import password reset validator
+const { userSignupValidator, userSigninValidator, passwordResetValidator } = require('../validator');
+const { userById } = require('../controllers/user');
+
+const router = express.Router();
+
+router.post('/signup', userSignupValidator, signup);
+router.post('/signin', userSigninValidator, signin);
+router.get('/signout', signout);
