99b98cd1b759fcd3d480bf891245b16d2aac01bb
amanS2206 <aman.saxena_cs18@gla.ac.in>
2021-04-08 17:49:53 +0530

    update auth.js

--- nodeapi/routes/auth.js
+++ nodeapi/routes/auth.js
@@ -11,7 +11,6 @@ router.post('/signup', userSignupValidator, signup);
 router.post('/signin', userSigninValidator, signin);
 router.get('/signout', signout);
 
-
 // password forgot and reset routes
 router.put('/forgot-password', forgotPassword);
 router.put('/reset-password', passwordResetValidator, resetPassword);
