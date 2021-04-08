bf4be3f154b02c965bcd3e0b51bf807eaa170d4a
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2021-04-08 14:57:23 +0530

    Update user.js

--- nodeapi/controllers/user.js
+++ nodeapi/controllers/user.js
@@ -18,3 +18,21 @@ exports.userById = (req, res, next, id) => {
             next();
         });
 };
+
+
+exports.hasAuthorization = (req, res, next) => {
+    let sameUser = req.profile && req.auth && req.profile._id == req.auth._id;
+    let adminUser = req.profile && req.auth && req.auth.role === 'admin';
+
+    const authorized = sameUser || adminUser;
+
+    // console.log("req.profile ", req.profile, " req.auth ", req.auth);
+    // console.log("SAMEUSER", sameUser, "ADMINUSER", adminUser);
+
+    if (!authorized) {
+        return res.status(403).json({
+            error: 'User is not authorized to perform this action'
+        });
+    }
+    next();
+};
