3138ca722013e0f63b952c595c7edf6c100bbe4d
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2021-04-08 16:33:01 +0530

    Update user.js

--- nodeapi/controllers/user.js
+++ nodeapi/controllers/user.js
@@ -116,3 +116,16 @@ exports.updateUser = (req, res, next) => {
         });
     });
 };
+
+
+
+
+exports.userPhoto = (req, res, next) => {
+    if (req.profile.photo.data) {
+        res.set(('Content-Type', req.profile.photo.contentType));
+        return res.send(req.profile.photo.data);
+    }
+    next();
+};
+
+
