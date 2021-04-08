6ad35fec00adf3237f66326b135ec8516db86ce3
amanS2206 <aman.saxena_cs18@gla.ac.in>
2021-04-08 17:51:26 +0530

    update post.js

---
+++ nodeapi/routes/post.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const {
+    getPosts,
+    createPost,
+    postsByUser,
+    postById,
+    isPoster,
+    updatePost,
+    deletePost,
+    photo,
+    singlePost,
+    like,
+    unlike,
+    comment,
+    uncomment,
+    updateComment
+} = require('../controllers/post');
+const { requireSignin } = require('../controllers/auth');
+const { userById } = require('../controllers/user');
+const { createPostValidator } = require('../validator');
