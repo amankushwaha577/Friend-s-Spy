38863918363cb469e11fad71f7dacab92ea36d72
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2021-04-08 14:51:44 +0530

    Update post.js

--- nodeapi/controllers/post.js
+++ nodeapi/controllers/post.js
@@ -69,3 +69,37 @@ exports.getPosts = async (req, res) => {
         })
         .catch(err => console.log(err));
 };
+
+
+
+
+
+exports.createPost = (req, res, next) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            return res.status(400).json({
+                error: 'Image could not be uploaded'
+            });
+        }
+        let post = new Post(fields);
+
+        req.profile.hashed_password = undefined;
+        req.profile.salt = undefined;
+        post.postedBy = req.profile;
+
+        if (files.photo) {
+            post.photo.data = fs.readFileSync(files.photo.path);
+            post.photo.contentType = files.photo.type;
+        }
+        post.save((err, result) => {
+            if (err) {
+                return res.status(400).json({
+                    error: err
+                });
+            }
+            res.json(result);
+        });
+    });
+};
