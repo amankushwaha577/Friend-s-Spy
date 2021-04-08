dd2d12093a1e9f026e5622e5f994271e02b83022
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2021-04-08 14:50:45 +0530

    Create post.js

---
+++ nodeapi/controllers/post.js
@@ -0,0 +1,21 @@
+const Post = require('../models/post');
+const formidable = require('formidable');
+const fs = require('fs');
+const _ = require('lodash');
+
+exports.postById = (req, res, next, id) => {
+    Post.findById(id)
+        .populate('postedBy', '_id name')
+        .populate('comments.postedBy', '_id name')
+        .populate('postedBy', '_id name role')
+        .select('_id title body created likes comments photo')
+        .exec((err, post) => {
+            if (err || !post) {
+                return res.status(400).json({
+                    error: err
+                });
+            }
+            req.post = post;
+            next();
+        });
+};
