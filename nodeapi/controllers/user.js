cebe51f46d7a780bda56be05fb666095eaf9093d
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2021-04-08 16:36:27 +0530

    Update user.js

--- nodeapi/controllers/user.js
+++ nodeapi/controllers/user.js
@@ -180,3 +180,20 @@ exports.removeFollowing = (req, res, next) => {
         next();
     });
 };
+
+
+exports.removeFollower = (req, res) => {
+    User.findByIdAndUpdate(req.body.unfollowId, { $pull: { followers: req.body.userId } }, { new: true })
+        .populate('following', '_id name')
+        .populate('followers', '_id name')
+        .exec((err, result) => {
+            if (err) {
+                return res.status(400).json({
+                    error: err
+                });
+            }
+            result.hashed_password = undefined;
+            result.salt = undefined;
+            res.json(result);
+        });
+};
