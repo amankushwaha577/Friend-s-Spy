571bb3899cc75eecdf0adef6503e14154ec11931
Akshat <59647105+Akshat555624@users.noreply.github.com>
2021-04-12 19:36:55 +0530

    Update user.js

--- nodeapi/models/user.js
+++ nodeapi/models/user.js
@@ -3,3 +3,23 @@ const uuidv1 = require("uuid/v1");
 const crypto = require("crypto");
 const { ObjectId } = mongoose.Schema;
 const Post = require("./post");
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    hashed_password: {
+        type: String,
+        required: true
+    },
+    salt: String,
+    created: {
+        type: Date,
+        default: Date.now
+    },
