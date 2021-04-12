8446b25a6bb0409cb653452c7f2cacb7afc07769
Akshat <59647105+Akshat555624@users.noreply.github.com>
2021-04-12 19:31:43 +0530

    Create post.js

---
+++ nodeapi/models/post.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Schema;
+
+const postSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    photo: {
+        data: Buffer,
+        contenType: String
