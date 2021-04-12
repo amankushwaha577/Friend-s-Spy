85ab4d2c8c6c913cb09bcfde6f7783b0c6458c6a
Akshat <59647105+Akshat555624@users.noreply.github.com>
2021-04-12 19:40:46 +0530

    Update index.js

--- nodeapi/validator/index.js
+++ nodeapi/validator/index.js
@@ -21,3 +21,14 @@ exports.createPostValidator = (req, res, next) => {
     // proceed to next middleware
     next();
 };
+exports.userSignupValidator = (req, res, next) => {
+    // name is not null and between 4-10 characters
+    req.check('name', 'Name is required').notEmpty();
+    // email is not null, valid and normalized
+    req.check('email', 'Email must be between 3 to 32 characters')
+        .matches(/.+\@.+\..+/)
+        .withMessage('Email must contain @')
+        .isLength({
+            min: 4,
+            max: 2000
+        });
