96c76d55c4afa5f67a5c61181f64970c7c666c0d
Akshat <59647105+Akshat555624@users.noreply.github.com>
2021-04-12 19:38:28 +0530

    Update user.js

--- nodeapi/models/user.js
+++ nodeapi/models/user.js
@@ -83,3 +83,10 @@ userSchema.methods = {
         }
     }
 };
+// pre middleware
+userSchema.pre("remove", function(next) {
+    Post.remove({ postedBy: this._id }).exec();
+    next();
+});
+
+module.exports = mongoose.model("User", userSchema);
