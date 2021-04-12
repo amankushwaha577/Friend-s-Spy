2269416c4064dec5e2b0cf314c3dc7db758f20bc
Akshat <59647105+Akshat555624@users.noreply.github.com>
2021-04-12 19:41:30 +0530

    Update index.js

--- nodeapi/validator/index.js
+++ nodeapi/validator/index.js
@@ -50,3 +50,26 @@ exports.userSignupValidator = (req, res, next) => {
     // proceed to next middleware
     next();
 };
+exports.userSigninValidator = (request, response, next) => {
+    request
+        .check('email', 'Email must be between 3 to 32 characters')
+        .matches(
+            /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
+        )
+        .withMessage('Please type your valid email address')
+        .isLength({
+            min: 4,
+            max: 32
+        });
+    request.check('password', 'Invalid Social Login Token!').notEmpty();
+    request
+        .check('password')
+        .isLength({ min: 6 })
+        .withMessage('Your social login token is invalid!');
+    const errors = request.validationErrors();
+    if (errors) {
+        const firstError = errors.map(error => error.msg)[0];
+        return res.status(400).json({ error: firstError });
+    }
+    next();
+};
