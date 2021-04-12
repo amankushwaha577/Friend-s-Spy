2e38293ba149e07e1b477e65d20b4b53cac1a105
Akshat <59647105+Akshat555624@users.noreply.github.com>
2021-04-12 19:41:09 +0530

    Update index.js

--- nodeapi/validator/index.js
+++ nodeapi/validator/index.js
@@ -32,3 +32,21 @@ exports.userSignupValidator = (req, res, next) => {
             min: 4,
             max: 2000
         });
+
+         // check for password
+    req.check('password', 'Password is required').notEmpty();
+    req.check('password')
+        .isLength({ min: 6 })
+        .withMessage('Password must contain at least 6 characters')
+        .matches(/\d/)
+        .withMessage('Password must contain a number');
+    // check for errors
+    const errors = req.validationErrors();
+    // if error show the first one as they happen
+    if (errors) {
+        const firstError = errors.map(error => error.msg)[0];
+        return res.status(400).json({ error: firstError });
+    }
+    // proceed to next middleware
+    next();
+};
