70e8a9d6e6ce392a6c751a3636a24340fdceb82c
Harshit Saxena <harshit.saxena_cs18@gla.ac.in>
2021-04-14 17:07:16 +0530

    created signout function

--- nodeapi/auth/index.js
+++ nodeapi/auth/index.js
@@ -40,3 +40,20 @@ export const setName = (name, next) => {
         next();
     }
 };
+
+export const signout = next => {
+    if (typeof window !== 'undefined') localStorage.removeItem('jwt');
+    next();
+    return fetch(`${process.env.REACT_APP_API_URL}/signout`, {
+        method: 'GET'
+    })
+        .then(response => {
+            console.log('signout', response);
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+
+
+
