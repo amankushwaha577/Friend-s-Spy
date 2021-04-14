f00218cfffb45fb5e09e9dec8dbfba8d1d04f384
akarshit2001 <akarshitsrivastava@gmail.com>
2021-04-14 13:51:35 +0530

    user.js is completed

--- models/user.js
+++ models/user.js
@@ -66,3 +66,29 @@ userSchema
     .get(function() {
         return this._password;
     });
+// methods
+userSchema.methods = {
+    authenticate: function(plainText) {
+        return this.encryptPassword(plainText) === this.hashed_password;
+    },
+
+    encryptPassword: function(password) {
+        if (!password) return "";
+        try {
+            return crypto
+                .createHmac("sha1", this.salt)
+                .update(password)
+                .digest("hex");
+        } catch (err) {
+            return "";
+        }
+    }
+};
+
+// pre middleware
+userSchema.pre("remove", function(next) {
+    Post.remove({ postedBy: this._id }).exec();
+    next();
+});
+
+module.exports = mongoose.model("User", userSchema);
