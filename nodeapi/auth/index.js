f2dbfe70678a9a765285365ac07ae904765cdf09
Harshit Saxena <harshit.saxena_cs18@gla.ac.in>
2021-04-14 17:10:03 +0530

    update index.js

--- nodeapi/auth/index.js
+++ nodeapi/auth/index.js
@@ -55,5 +55,34 @@ export const signout = next => {
 };
 
 
+export const isAuthenticated = () => {
+    if (typeof window == 'undefined') {
+        return false;
+    }
+
+    if (localStorage.getItem('jwt')) {
+        return JSON.parse(localStorage.getItem('jwt'));
+    } else {
+        return false;
+    }
+};
+
+export const forgotPassword = email => {
+    console.log('email: ', email);
+    return fetch(`${process.env.REACT_APP_API_URL}/forgot-password/`, {
+        method: 'PUT',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email })
+    })
+        .then(response => {
+            console.log('forgot password response: ', response);
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
 
 
