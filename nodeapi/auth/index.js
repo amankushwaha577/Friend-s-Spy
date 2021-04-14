10b573ec9203b357651a0a59dac5a1bd2932aaad
Harshit Saxena <harshit.saxena_cs18@gla.ac.in>
2021-04-14 17:05:42 +0530

    created signin function

--- nodeapi/auth/index.js
+++ nodeapi/auth/index.js
@@ -12,3 +12,18 @@ export const signup = user => {
         })
         .catch(err => console.log(err));
 };
+
+export const signin = user => {
+    return fetch(`${process.env.REACT_APP_API_URL}/signin`, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
