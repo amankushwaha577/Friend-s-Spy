a7e90ecf88a5623170e5e337a62bad598a10a77f
akarshit2001 <akarshitsrivastava@gmail.com>
2021-04-14 13:31:51 +0530

    userschema is created ...

--- models/user.js
+++ models/user.js
@@ -3,3 +3,50 @@ const uuidv1 = require("uuid/v1");
 const crypto = require("crypto");
 const { ObjectId } = mongoose.Schema;
 const Post = require("./post");
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    hashed_password: {
+        type: String,
+        required: true
+    },
+    salt: String,
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    updated: Date,
+    photo: {
+        data: Buffer,
+        contentType: String
+    },
+    about: {
+        type: String,
+        trim: true
+    },
+    following: [{ type: ObjectId, ref: "User" }],
+    followers: [{ type: ObjectId, ref: "User" }],
+    resetPasswordLink: {
+        data: String,
+        default: ""
+    },
+    role: {
+        type: String,
+        default: "subscriber"
+    }
+});
+
+/**
+ * Virtual fields are additional fields for a given model.
+ * Their values can be set manually or automatically with defined functionality.
+ * Keep in mind: virtual properties (password) don’t get persisted in the database.
+ * They only exist logically and are not written to the document’s collection.
+ */
