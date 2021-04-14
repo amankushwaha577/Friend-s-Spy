5fd4fb072837ef4cee2eb38e43b226774252450f
Harshit Saxena <harshit.saxena_cs18@gla.ac.in>
2021-04-14 17:10:45 +0530

    created reset Password and social login function

--- nodeapi/auth/index.js
+++ nodeapi/auth/index.js
@@ -86,3 +86,35 @@ export const forgotPassword = email => {
 
 
 
+export const resetPassword = resetInfo => {
+    return fetch(`${process.env.REACT_APP_API_URL}/reset-password/`, {
+        method: 'PUT',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(resetInfo)
+    })
+        .then(response => {
+            console.log('forgot password response: ', response);
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+export const socialLogin = user => {
+    return fetch(`${process.env.REACT_APP_API_URL}/social-login/`, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        // credentials: "include", // works only in the same origin
+        body: JSON.stringify(user)
+    })
+        .then(response => {
+            console.log('signin response: ', response);
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
