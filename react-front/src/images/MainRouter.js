407ee4f7f2d39385c766ffc3acc442734be2357b
akarshit2001 <akarshitsrivastava@gmail.com>
2021-04-15 22:03:15 +0530

    MainRouter.js is updated.

---
+++ react-front/src/images/MainRouter.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./core/Home";
+import Menu from "./core/Menu";
+import Signup from "./user/Signup";
+import Signin from "./user/Signin";
+import Profile from "./user/Profile";
+import Users from "./user/Users";
+import EditProfile from "./user/EditProfile";
+import FindPeople from "./user/FindPeople";
+import NewPost from "./post/NewPost";
+import EditPost from "./post/EditPost";
+import SinglePost from "./post/SinglePost";
+import PrivateRoute from "./auth/PrivateRoute";
+import ForgotPassword from "./user/ForgotPassword";
+import ResetPassword from "./user/ResetPassword";
+import Admin from "./admin/Admin"
