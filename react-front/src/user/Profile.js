5a3da3c693a1494ec267afbd13457af8da23b3be
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2021-04-17 17:08:43 +0530

    Create Profile.js

---
+++ react-front/src/user/Profile.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { isAuthenticated } from "../auth";
+import { Redirect, Link } from "react-router-dom";
+import { read } from "./apiUser";
+import DefaultProfile from "../images/avatar.jpg";
+import DeleteUser from "./DeleteUser";
+import FollowProfileButton from "./FollowProfileButton";
+import ProfileTabs from "./ProfileTabs";
+import { listByUser } from "../post/apiPost";
+
+class Profile extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: { following: [], followers: [] },
+      redirectToSignin: false,
+      following: false,
+      error: "",
+      posts: []
+    };
+  }
