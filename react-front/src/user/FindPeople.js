398c43ed51d9b9bb59d4f4d6dc4a314f90e0b211
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2021-04-17 17:03:58 +0530

    Update FindPeople.js

--- react-front/src/user/FindPeople.js
+++ react-front/src/user/FindPeople.js
@@ -26,3 +26,38 @@ class FindPeople extends Component {
             }
         });
     }
+    
+    renderUsers = users => (
+        <div className="row">
+            {users.map((user, i) => (
+                <div className="card col-md-4" key={i}>
+                    <img
+                        style={{ height: "200px", width: "auto" }}
+                        className="img-thumbnail"
+                        src={`${process.env.REACT_APP_API_URL}/user/photo/${
+                            user._id
+                        }`}
+                        onError={i => (i.target.src = `${DefaultProfile}`)}
+                        alt={user.name}
+                    />
+                    <div className="card-body">
+                        <h5 className="card-title">{user.name}</h5>
+                        <p className="card-text">{user.email}</p>
+                        <Link
+                            to={`/user/${user._id}`}
+                            className="btn btn-raised btn-primary btn-sm"
+                        >
+                            View Profile
+                        </Link>
+
+                        <button
+                            onClick={() => this.clickFollow(user, i)}
+                            className="btn btn-raised btn-info float-right btn-sm"
+                        >
+                            Follow
+                        </button>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
