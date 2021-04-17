f10c9470e9c02b6c9ecc68d31a3bbde4b7d22a34
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2021-04-17 17:05:41 +0530

    Update FollowProfileButton.js

--- react-front/src/user/FollowProfileButton.js
+++ react-front/src/user/FollowProfileButton.js
@@ -9,3 +9,30 @@ class FollowProfileButton extends Component {
     unfollowClick = () => {
         this.props.onButtonClick(unfollow);
     };
+
+    
+
+    render() {
+        return (
+            <div className="d-inline-block">
+                {!this.props.following ? (
+                    <button
+                        onClick={this.followClick}
+                        className="btn btn-success btn-raised mr-5"
+                    >
+                        Follow
+                    </button>
+                ) : (
+                    <button
+                        onClick={this.unfollowClick}
+                        className="btn btn-warning btn-raised"
+                    >
+                        UnFollow
+                    </button>
+                )}
+            </div>
+        );
+    }
+}
+
+export default FollowProfileButton;
