c3446aea9961dbbd681db16ad6e4bf359f75e70c
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2021-04-17 17:21:03 +0530

    Update SocialLogin.js

--- react-front/src/user/SocialLogin.js
+++ react-front/src/user/SocialLogin.js
@@ -17,3 +17,41 @@ class SocialLogin extends Component {
         const user = {
             tokenId: tokenId
         };
+
+
+        socialLogin(user).then(data => {
+            // console.log('signin data: ', data);
+            if (data.error) {
+                console.log('Error Login. Please try again..');
+            } else {
+                // console.log('signin success - setting jwt: ', data);
+                authenticate(data, () => {
+                    console.log('social login response from api', data);
+                    this.setState({ redirectToReferrer: true });
+                });
+            }
+        });
+    };
+
+
+    
+    
+    render() {
+        // redirect
+        const { redirectToReferrer } = this.state;
+        if (redirectToReferrer) {
+            return <Redirect to="/" />;
+        }
+
+        return (
+            <GoogleLogin
+                clientId="679380407525-2cvoah9gpsjjffc5k1p6atahhf2vqfl4.apps.googleusercontent.com"
+                buttonText="Login with Google"
+                onSuccess={this.responseGoogle}
+                onFailure={this.responseGoogle}
+            />
+        );
+    }
+}
+
+export default SocialLogin;
