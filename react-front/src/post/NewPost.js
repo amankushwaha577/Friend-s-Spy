09c7738124c43595fdbe9a8410898103c5f85f26
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2021-04-17 17:34:07 +0530

    Create NewPost.js

---
+++ react-front/src/post/NewPost.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { isAuthenticated } from "../auth";
+import { create } from "./apiPost";
+import { Redirect } from "react-router-dom";
+
+class NewPost extends Component {
+    constructor() {
+        super();
+        this.state = {
+            title: "",
+            body: "",
+            photo: "",
+            error: "",
+            user: {},
+            fileSize: 0,
+            loading: false,
+            redirectToProfile: false
+        };
+    }
