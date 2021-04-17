c9646080e2720744e3dafec0eb07765f7d313c15
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2021-04-17 17:06:57 +0530

    Update ForgotPassword.js

--- react-front/src/user/ForgotPassword.js
+++ react-front/src/user/ForgotPassword.js
@@ -21,3 +21,49 @@ class ForgotPassword extends Component {
             }
         });
     };
+
+
+    
+    render() {
+        return (
+            <div className="container">
+                <h2 className="mt-5 mb-5">Ask for Password Reset</h2>
+
+                {this.state.message && (
+                    <h4 className="bg-success">{this.state.message}</h4>
+                )}
+                {this.state.error && (
+                    <h4 className="bg-warning">{this.state.error}</h4>
+                )}
+
+                <form>
+                    <div className="form-group mt-5">
+                        <input
+                            type="email"
+                            className="form-control"
+                            placeholder="Your email address"
+                            value={this.state.email}
+                            name="email"
+                            onChange={e =>
+                                this.setState({
+                                    email: e.target.value,
+                                    message: "",
+                                    error: ""
+                                })
+                            }
+                            autoFocus
+                        />
+                    </div>
+                    <button
+                        onClick={this.forgotPassword}
+                        className="btn btn-raised btn-primary"
+                    >
+                        Send Password Rest Link
+                    </button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default ForgotPassword;
