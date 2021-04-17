0951d679d70f7118fca33b39c35c6db47ddf85fb
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2021-04-17 17:14:47 +0530

    Create Signin.js

---
+++ react-front/src/user/Signin.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Link, Redirect } from "react-router-dom";
+import { signin, authenticate } from "../auth";
+import SocialLogin from "./SocialLogin";
+
+class Signin extends Component {
+    constructor() {
+        super();
+        this.state = {
+            email: "",
+            password: "",
+            error: "",
+            redirectToReferer: false,
+            loading: false,
+            recaptcha: false
+        };
+    }
+
+    handleChange = name => event => {
+        this.setState({ error: "" });
+        this.setState({ [name]: event.target.value });
+    };
