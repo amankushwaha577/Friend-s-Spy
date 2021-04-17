feebbff98c9af738fdcf1044897cb5457a0341e3
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2021-04-17 17:30:35 +0530

    Update Comment.js

--- react-front/src/post/Comment.js
+++ react-front/src/post/Comment.js
@@ -67,3 +67,111 @@ class Comment extends Component {
             }
         });
     };
+
+
+    deleteConfirmed = comment => {
+        let answer = window.confirm(
+            "Are you sure you want to delete your comment?"
+        );
+        if (answer) {
+            this.deleteComment(comment);
+        }
+    };
+
+    render() {
+        const { comments } = this.props;
+        const { error } = this.state;
+
+        return (
+            <div>
+                <h2 className="mt-5 mb-5">Leave a comment</h2>
+
+                <form onSubmit={this.addComment}>
+                    <div className="form-group">
+                        <input
+                            type="text"
+                            onChange={this.handleChange}
+                            value={this.state.text}
+                            className="form-control"
+                            placeholder="Leave a comment..."
+                        />
+                        <button className="btn btn-raised btn-success mt-2">
+                            Post
+                        </button>
+                    </div>
+                </form>
+
+                <div
+                    className="alert alert-danger"
+                    style={{ display: error ? "" : "none" }}
+                >
+                    {error}
+                </div>
+
+                <div className="col-md-12">
+                    <h3 className="text-primary">{comments.length} Comments</h3>
+                    <hr />
+                    {comments.map((comment, i) => (
+                        <div key={i}>
+                            <div>
+                                <Link to={`/user/${comment.postedBy._id}`}>
+                                    <img
+                                        style={{
+                                            borderRadius: "50%",
+                                            border: "1px solid black"
+                                        }}
+                                        className="float-left mr-2"
+                                        height="30px"
+                                        width="30px"
+                                        onError={i =>
+                                            (i.target.src = `${DefaultProfile}`)
+                                        }
+                                        src={`${
+                                            process.env.REACT_APP_API_URL
+                                        }/user/photo/${comment.postedBy._id}`}
+                                        alt={comment.postedBy.name}
+                                    />
+                                </Link>
+                                <div>
+                                    <p className="lead">{comment.text}</p>
+                                    <p className="font-italic mark">
+                                        Posted by{" "}
+                                        <Link
+                                            to={`/user/${comment.postedBy._id}`}
+                                        >
+                                            {comment.postedBy.name}{" "}
+                                        </Link>
+                                        on{" "}
+                                        {new Date(
+                                            comment.created
+                                        ).toDateString()}
+                                        <span>
+                                            {isAuthenticated().user &&
+                                                isAuthenticated().user._id ===
+                                                    comment.postedBy._id && (
+                                                    <>
+                                                        <span
+                                                            onClick={() =>
+                                                                this.deleteConfirmed(
+                                                                    comment
+                                                                )
+                                                            }
+                                                            className="text-danger float-right mr-1"
+                                                        >
+                                                            Remove
+                                                        </span>
+                                                    </>
+                                                )}
+                                        </span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Comment;
