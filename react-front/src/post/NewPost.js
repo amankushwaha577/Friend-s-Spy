2589529ee1d9cddd327468df0f20905465060c99
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2021-04-17 17:34:22 +0530

    Update NewPost.js

--- react-front/src/post/NewPost.js
+++ react-front/src/post/NewPost.js
@@ -17,3 +17,24 @@ class NewPost extends Component {
             redirectToProfile: false
         };
     }
+
+    componentDidMount() {
+        this.postData = new FormData();
+        this.setState({ user: isAuthenticated().user });
+    }
+
+    isValid = () => {
+        const { title, body, fileSize } = this.state;
+        if (fileSize > 100000) {
+            this.setState({
+                error: "File size should be less than 100kb",
+                loading: false
+            });
+            return false;
+        }
+        if (title.length === 0 || body.length === 0) {
+            this.setState({ error: "All fields are required", loading: false });
+            return false;
+        }
+        return true;
+    };
