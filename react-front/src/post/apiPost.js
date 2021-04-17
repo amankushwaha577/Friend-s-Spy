775661fbd6a99fca11d3691f51a9f16eb3185764
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2021-04-17 17:25:29 +0530

    Create apiPost.js

---
+++ react-front/src/post/apiPost.js
@@ -0,0 +1,24 @@
+export const create = (userId, token, post) => {
+    return fetch(`${process.env.REACT_APP_API_URL}/post/new/${userId}`, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: post
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+// export const list = () => {
+//     return fetch(`${process.env.REACT_APP_API_URL}/posts`, {
+//         method: "GET"
+//     })
+//         .then(response => {
+//             return response.json();
+//         })
+//         .catch(err => console.log(err));
+// };
