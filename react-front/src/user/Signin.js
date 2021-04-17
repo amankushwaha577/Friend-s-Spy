6866fbb67df8d0c1db23f60438d2325545725bc4
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2021-04-17 17:15:09 +0530

    Update Signin.js

--- react-front/src/user/Signin.js
+++ react-front/src/user/Signin.js
@@ -20,3 +20,35 @@ class Signin extends Component {
         this.setState({ error: "" });
         this.setState({ [name]: event.target.value });
     };
+
+    recaptchaHandler = e => {
+        this.setState({ error: "" });
+        let userDay = e.target.value.toLowerCase();
+        let dayCount;
+
+        if (userDay === "sunday") {
+            dayCount = 0;
+        } else if (userDay === "monday") {
+            dayCount = 1;
+        } else if (userDay === "tuesday") {
+            dayCount = 2;
+        } else if (userDay === "wednesday") {
+            dayCount = 3;
+        } else if (userDay === "thursday") {
+            dayCount = 4;
+        } else if (userDay === "friday") {
+            dayCount = 5;
+        } else if (userDay === "saturday") {
+            dayCount = 6;
+        }
+
+        if (dayCount === new Date().getDay()) {
+            this.setState({ recaptcha: true });
+            return true;
+        } else {
+            this.setState({
+                recaptcha: false
+            });
+            return false;
+        }
+    };
