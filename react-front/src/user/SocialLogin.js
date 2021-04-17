f2ff1a5ad211cb857a57cd043aa8cefc705790fa
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2021-04-17 17:20:32 +0530

    Create SocialLogin.js

---
+++ react-front/src/user/SocialLogin.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import GoogleLogin from 'react-google-login';
+import { socialLogin, authenticate } from '../auth';
+
+class SocialLogin extends Component {
+    constructor() {
+        super();
+        this.state = {
+            redirectToReferrer: false
+        };
+    }
+
+    responseGoogle = response => {
+        // console.log('response', response);
+        const tokenId = response.tokenId;
+        const user = {
+            tokenId: tokenId
+        };
