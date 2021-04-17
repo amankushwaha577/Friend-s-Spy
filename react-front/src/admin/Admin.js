e85a50c1e1a00b6154f113c01f7a4cdc6d7d34c3
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2021-04-17 17:22:14 +0530

    Update Admin.js

--- react-front/src/admin/Admin.js
+++ react-front/src/admin/Admin.js
@@ -14,3 +14,37 @@ class Admin extends Component {
             this.setState({ redirectToHome: true });
         }
     }
+
+
+    
+    render() {
+        if (this.state.redirectToHome) {
+            return <Redirect to="/" />;
+        }
+
+        return (
+            <div>
+                <div className="jumbotron">
+                    <h2>Admin Dashboard</h2>
+                    <p className="lead">Welcome to React Frontend</p>
+                </div>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <h2>Posts</h2>
+                            <hr />
+                            <Posts />
+                        </div>
+                        <div className="col-md-6">
+                            <h2>Users</h2>
+                            <hr />
+                            <Users />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Admin;
