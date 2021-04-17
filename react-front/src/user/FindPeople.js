eaa89689ee25f4aaaf63ef4f106143082f438fd6
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2021-04-17 17:01:36 +0530

    Create FindPeople.js

---
+++ react-front/src/user/FindPeople.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { findPeople, follow } from "./apiUser";
+import DefaultProfile from "../images/avatar.jpg";
+import { Link } from "react-router-dom";
+import { isAuthenticated } from "../auth";
+
+class FindPeople extends Component {
+    constructor() {
+        super();
+        this.state = {
+            users: [],
+            error: "",
+            open: false
+        };
+    }
+
+    componentDidMount() {
+        const userId = isAuthenticated().user._id;
+        const token = isAuthenticated().token;
+
+        findPeople(userId, token).then(data => {
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                this.setState({ users: data });
+            }
+        });
+    }
