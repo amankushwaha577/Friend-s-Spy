afb03970f461feff88bdce73310a176faff27db2
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2023-09-02 19:55:15 +0530

    Users

--- react-front/src/user/Users.js
+++ react-front/src/user/Users.js
@@ -24,9 +24,10 @@ class Users extends Component {
     renderUsers = users => (
         <div className="row">
             {users.map((user, i) => (
-                <div className="card col-md-4" key={i}>
+                // <div className="card col-md-4" key={i}>
+                <div className="card mb-3" key={i} style={{flex: "0 0 33.33333%", maxWidth: "30%", marginRight: "3.33%"}}>
                     <img
-                        style={{ height: "200px", width: "auto" }}
+                        style={{ height: "300px", width: "auto" }}
                         className="img-thumbnail"
                         src={`${process.env.REACT_APP_API_URL}/user/photo/${
                             user._id
@@ -35,11 +36,12 @@ class Users extends Component {
                         alt={user.name}
                     />
                     <div className="card-body">
-                        <h5 className="card-title">{user.name}</h5>
-                        <p className="card-text">{user.email}</p>
+                        <h5 className="card-title" style={{fontFamily:"Bahnschrift SemiBold"}}>{user.name}</h5>
+                        <p className="card-text" style={{fontFamily:"Segoe Print"}}>{user.email}</p>
                         <Link
                             to={`/user/${user._id}`}
                             className="btn btn-raised btn-primary btn-sm"
+                            style={{fontFamily:"Century Gothic"}}
                         >
                             View Profile
                         </Link>
@@ -52,8 +54,8 @@ class Users extends Component {
     render() {
         const { users } = this.state;
         return (
-            <div className="container">
-                <h2 className="mt-5 mb-5">Users</h2>
+            <div className="container" style = {{marginTop:"100px", fontFamily:"Copperplate Gothic Light"}}>
+                <h1 className="mt-5 mb-5 text-white font-weight-bold">Users</h1>
 
                 {this.renderUsers(users)}
             </div>
